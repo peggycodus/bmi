2f398e11b4188ca00911c130c3385826ed045bad
Peggy Condon <peggyc3@gmail.com>
2014-07-20 23:34:35 -0700

    commented out alert specs

--- js/specs.js
+++ js/specs.js
@@ -5,7 +5,7 @@ describe('bmiCalculatorEnglish', function() {
   it('calculates the BMI from the  weight in pounds and the height in inches', function() {
     bmiCalculatorEnglish(365, 72).should.equal(49.5));
   });
-  it('should return an alert when  the weight or the height are zero', function() {
+  /*it('should return an alert when  the weight or the height are zero', function() {
     bmiCalculatorEnglish(365, 0).should.equal(alert));
   });
   it('should return an alert when  the weight or the height are zero', function() {
@@ -13,7 +13,5 @@ describe('bmiCalculatorEnglish', function() {
   });
   it('should return an alert when  the weight or the height are zero', function() {
     bmiCalculatorEnglish(0, 0).should.equal(alert));
-  });
-    
-
+  });*/
 });
